8373583bc99ee239f97088e33e3fa7cbaec33a6d
Rachit Kanaujia <rachit.kanaujia@geminisolutions.in>
2022-04-23 12:50:13 +0530

    full project

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup } from '@angular/forms'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm: FormGroup
+  constructor(
+    private fb: FormBuilder,
+    private _http: HttpClient,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: [''],
+      password: [''],
+    })
   }
 
+  //LOGIN METHOD
+  login() {
+    this._http.get<any>('http://localhost:3000/users').subscribe(
+      (res) => {
+        const user = res.find((a: any) => {
+          return (
+            a.email === this.loginForm.value.email &&
+            a.password === this.loginForm.value.password
+          )
+        })
+        if (user) {
+          console.log('login successfull!!')
+          this.loginForm.reset()
+          this.router.navigate(['dashboard'])
+        } else {
+          console.log('user not found.')
+        }
+      },
+      (err) => {
+        console.log('Error', err)
+      },
+    )
+  }
 }
